48164aa06f59a6ae636514344d76dd3b39665691
richard-remote <clarkeril8082@gmail.com>
2019-06-17 16:55:53 -0400

    Moving between screens Navigate function of navigation changes to route in the parentheses ***If it's already on the stack, nothing happens this.props.navigation.navigate('RouteName') only changes to that route if it's not already there this.props.navigation.push('RouteName') adds the route to the stack this.props.navigation.popToTop() returns to the first screen of the stack this.props.navigation.goBack() returns to the previous route on the stack

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 //HomeScreen Component
@@ -8,17 +8,39 @@ class HomeScreen extends Component {
     return (
       <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
         <Text>Home Screen</Text>
+        <Button 
+          title= 'Go to Details'
+          onPress={() => this.props.navigation.navigate('Details') }
+        />
       </View>
     );
   }
 }
 
 //DetailsScreen Component
+//Navigate function of navigation changes to route in the parentheses
+// If it's already on the stack, nothing happens
+//this.props.navigation.navigate('RouteName') only changes to that route if it's not already there
+//this.props.navigation.push('RouteName') adds the route to the stack 
+//this.props.navigation.popToTop() returns to the first screen of the stack
+//this.props.navigation.goBack() returns to the previous route on the stack
 class DetailsScreen extends Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>Details Screen</Text>
+        <Button 
+          title= 'Go Home'
+          onPress={()=> this.props.navigation.popToTop()}
+        />
+        <Button
+          title= 'Go Back'
+          onPress= {() => this.props.navigation.goBack()}
+        />
+        <Button
+          title= 'Push Details'
+          onPress= {()=> this.props.navigation.push('Details')}
+        />
       </View>
     );
   }
