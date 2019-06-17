b06dcbb31aa6085f73b353f2c011f7cd479cb659
richard-remote <clarkeril8082@gmail.com>
2019-06-17 14:49:06 -0400

    StackNavigator and initial route Created a stack navigator Added a second route, DetailsScreen Configured 'Home' as the inital route

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { View, Text } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
+//HomeScreen Component
 class HomeScreen extends Component {
   render() {
     return (
@@ -12,10 +13,34 @@ class HomeScreen extends Component {
   }
 }
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen
-  },
-});
+//DetailsScreen Component
+class DetailsScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
 
-export default createAppContainer(AppNavigator);
+//Exports the root stack navigator
+//Two routes: Home and Details
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen
+  },
+  {
+    initalRouteName: 'Home'
+  }
+
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends Component {
+  render(){
+    return <AppContainer />;
+  }
+}
